7a8dc26ca43a44444b8499d2bb3169535f1d6e1f
tariq <tariqabdelghani@gmail.com>
2019-08-27 22:51:35 +0200

    Solving DATABASE is not defined problem

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -14,8 +14,9 @@ import os
 # for heroku
 import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
+DATABASES = {'default': dj_database_url.config(conn_max_age=500)}
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
