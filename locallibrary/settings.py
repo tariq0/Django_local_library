0d9c747932309690ee80171a624436e29f9900d2
tariq <tariqabdelghani@gmail.com>
2019-08-27 23:18:27 +0200

    allowed hosts setting

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
